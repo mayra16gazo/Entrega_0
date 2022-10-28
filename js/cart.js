0a855b8116fce9907f6f815a18cfc484b6817ffe
mayra16gazo <mayra16gazo@gmail.com>
2022-10-27 23:23:33 -0300

    calcular costos de envios

--- js/cart.js
+++ js/cart.js
@@ -1,124 +1,93 @@
 const URL_PRODUCTO = 'https://japceibal.github.io/emercado-api/user_cart/25801.json';
-let nombreProduc = document.getElementById('nombreProducto');
-let imgProduc = document.getElementById('imgProduc');
-let costoProduc = document.getElementById('costoProduc')
+
 const TABLECART = document.getElementById('tabla-body');
 
+let carrito = [];
 
-fetch(URL_PRODUCTO)
-.then(respuesta => respuesta.json())
-.then(datos =>{
-    let htmlcart ='';
-    for(let producto of datos.articles){
+
+function vercarrito(articles){
+    let htmlcart = "";
+    for(let producto of articles){
         htmlcart +=`
-                <tr>
-                    <td id="imgProduc"><img src="${producto.image}" class="img-thumbnail col-7" "></th>
-                    <td id="nombreProducto">${producto.name}</th>
-                    <td id="costoProducto">${producto.currency + " " + producto.unitCost}</th>
-                    <td><input id="input" style="width:50px" value ="${producto.count}" min="1" type = "number" onclick ="calcular(this.value)"></th>
-                    <td><strong><p id="subtotal">USD ${producto.unitCost}</p></strong></td>
-                 
-                </tr>
+            <tr>
+                <td id="imgProduc"><img src="${producto.image}" class="img-thumbnail col-7" "></th>
+                <td id="nombreProducto">${producto.name}</th>
+                <td id="costoProducto">${producto.currency + " " + producto.unitCost}</th>
+                <td><input id="input" style="width:50px" value ="${producto.count}" min="1" type = "number" onclick ="calcular(this.value)"></th>
+                <td id=subtotaal><strong><p id="subtotal">USD ${producto.unitCost}</p></strong></td>
+                         
+            </tr>`
                 
-        `
-		
-    
-	
-    TABLECART.innerHTML = htmlcart;
-	}
+            
+            
+             TABLECART.innerHTML = htmlcart;
+        }
 
-    
-	
-
-})
-
+}
 
 function calcular(unidades){
-    fetch(URL_PRODUCTO)
-    .then(respuesta => respuesta.json())
-	.then(datos =>{
-		for(let producto of datos.articles){
-			var v = producto.unitCost*unidades;
-			v = producto.currency + " " + v
-			document.getElementById("subtotal").innerHTML = v;
-		}
-	})
+    for(let producto of carrito){
+    currency = producto.currency;
+    variante = producto.unitCost*unidades;
+    v = producto.currency + " " + variante
+    document.getElementById("subtotal").innerHTML = v;
+    }
+    costosfinales()
     
-				
 }
 
-let subTotal = document.getElementById("subtotal");
-
-
-document.getElementById('tipodeEnvio').addEventListener("change", function(e){
-    const id = e.target.id
-
-    if(id === 'Enviopremium'){
-
-    }
-})
-
-
-let subtotalCart = document.getElementById('subtotalCart');
-let costodeenvio = document.getElementById('shipping');
-let totalUSD = document.getElementById('totalCart');
 
 
 
-function tipodeenvio (){
-    if (document.getElementById('Enviopremium').checked){
-        costodeenvio = subTotal*0.15
-    }
-    else if (document.getElementById("Envioexpress").checked) {
-        costodeenvio= subTotal*0.07 
-    }
-    else if ( document.getElementById('Enviostandar').checked) {
-        costodeenvio = subTotal*0.05
-    }
-
-    document.getElementById('CostosEnvio').innerHTML = ``
+function costosfinales(){
+    let valordeEnvio = variante * p
+    document.getElementById('subtotalCart').innerHTML =  currency + " " + variante;
+    document.getElementById('costoenvio').innerHTML = currency + " " +  valordeEnvio ;
+    let suma = variante + valordeEnvio ;
+    document.getElementById('totalCart').innerHTML = currency + " " +  suma ;
 }
 
-// document.getElementById("productCost").innerHTML =`${currencyProducto} ${subtotal}`
-        
-//         if (document.getElementById("flexRadioDefault1").checked) {
-//             costoEnvio= subtotal*0.15
-//         } 
-//         else if (document.getElementById("flexRadioDefault2").checked) {
-//             costoEnvio= subtotal*0.07
-//         } 
-//         else if (document.getElementById("flexRadioDefault3").checked) {
-//             costoEnvio= subtotal*0.05
-//         }
-        
-//         document.getElementById("deliverCost").innerHTML =`${currencyProducto} ${Math.round(costoEnvio)}`
-              
-
-//     });
-
-// //Escucha modificación del tipo de envío seleccionado
-
-//     document.getElementById("deliverType").addEventListener("change", function(event){
-   
-//     const id = event.target.id
-      
-
-//     if (id === 'flexRadioDefault1') {
-       
-//         costoEnvio = subtotal*0.15
-       
-//     }
-    
-//     else if (id === 'flexRadioDefault2'){
-//         costoEnvio = subtotal*0.07
-//     }
-
-//     else {
-//        costoEnvio = subtotal*0.05
-//     }    
-    
-
-//     document.getElementById("deliverCost").innerHTML =`${currencyProducto} ${Math.round(costoEnvio)}`
 
 
-// });
+
+
+document.addEventListener("DOMContentLoaded", function(){
+    getJSONData(URL_PRODUCTO).then(function(resultObj){
+        if(resultObj.status === "ok"){
+            carrito = resultObj.data.articles;
+            vercarrito(carrito);
+            costosfinales();
+        }
+    })
+});
+
+
+document.getElementById('Enviopremium').addEventListener("change", function(){
+    p = 0.15;
+    costosfinales()
+});
+
+document.getElementById('Envioexpress').addEventListener("change", function(){
+    p = 0.07;
+    costosfinales()
+});
+
+document.getElementById('Enviostandar').addEventListener("change", function(){
+    p = 0.05;
+    costosfinales()
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
